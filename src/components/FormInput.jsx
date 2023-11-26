6931d6f24a707865c870d3c7cb40493c874602f8
Ankush <rodewad.ankush29@gmail.com>
2023-11-26 20:43:21 +0530

    added dropdown type input support in FormInput.jsx component

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
-export default function FormInput({type, label, placeHolder, onChangeCallback, valueState}){
+export default function FormInput({type, label, placeHolder, onChangeCallback, valueState, options =[]}){
     return (
         <div className="my-2">
             <label className='block text-gray-900 text-sm font-bold mb-1'>
                 {label}:
             </label>
             {
-                type && type === "checkbox" &&
-                <input id={label} name={label} type={type} className='inline-block py-2 px-3 text-gray-800 ' checked={valueState} onChange={e => onChangeCallback((prev) => { return !prev })}/>
-            }
-            {
-                type && type !== "checkbox" &&
-                <input placeholder={placeHolder} className='shadow-sm bg-white appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline' onChange={e => onChangeCallback(e.target.value)} type={type} id={label} name={label}/>
+                type && type === "checkbox" ?
+                    <input id={label} name={label} type={type} className='inline-block py-2 px-3 text-gray-800 ' checked={valueState} onChange={e => onChangeCallback((prev) => { return !prev })}/>
+                : type === "dropdown" ?
+                    <select id={label} name={label} className='shadow-sm bg-white appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:shadow-outline' onChange={e => onChangeCallback(e.target.value)}>
+                        <option value={""}>-- select --</option>
+                        {
+                            options && options.map(item => {
+                                return <option value={item} key={item}>{item}</option>
+                            })
+                        }
+                    </select>
+                :
+                    <input placeholder={placeHolder} className='shadow-sm bg-white appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:shadow-outline' onChange={e => onChangeCallback(e.target.value)} type={type} id={label} name={label}/>
             }
         </div>
     )
