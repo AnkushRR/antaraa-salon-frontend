e674d623287405521f349be893c13f39a1e807ab
Ankush <rodewad.ankush29@gmail.com>
2023-11-26 20:43:36 +0530

    added styling to 404 page

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function NoPage() {
     return (
-        <div className="not-found-page">
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
+        <div className="p-10 text-center space-y-3">
+            <h1 className={'text-4xl font-bold text-red-900'}>404</h1>
+            <h2 className={'text-lg text-black'}>Page Not Found</h2>
         </div>
     );
 }
