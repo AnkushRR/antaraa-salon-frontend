9f4b3e1ec2e7d40761dfa10d1439d6ee31bd9648
Ankush <rodewad.ankush29@gmail.com>
2024-01-11 21:34:24 +0530

    Add DateRangePicker to HomeStats component
    A DateRangePicker has been added to the HomeStats component. This implementation involves setting the initial state for startDate and endDate, calling the get_sale_stats_data function which retrieves sale stats based on the selected date range, and adding the DateRangePicker to the render method of the HomeStats component. Now, users will be able to interact with the DateRangePicker and see sales stats for the selected date range.

--- src/components/HomeStats.jsx
+++ src/components/HomeStats.jsx
@@ -3,6 +3,7 @@ import {getRequest} from "../utils/apiHandler.js";
 import StatsCard from "./StatsCard.jsx";
 import FormTitle from "./FormTitle.jsx";
 import NumberStatsCard from "./NumberStatsCard.jsx";
+import DateRangePicker from "./DateRangePicker.jsx";
 
 
 
@@ -13,6 +14,9 @@ export default function ({token, logoutFn, showNotification}) {
     const [saleToday, setSaleToday] = useState(null);
     const [sale7Days, setSale7Days] = useState({sales: 0, revenue: 0, profit: 0, loss: 0, customers: 0});
     const [saleThisMonth, setSaleThisMonth] = useState({sales: 0, revenue: 0, profit: 0, loss: 0, customers: 0});
+    const dateNow = new Date();
+    const [startDate, setStartDate] = useState(new Date(dateNow.getFullYear(), dateNow.getMonth() - 5, dateNow.getDate(), 23, 59, 59));
+    const [endDate, setEndDate] = useState(dateNow);
 
     async function getData(endpoint, callback, errorMessage) {
         const response = await getRequest(endpoint, {}, {token: `${token}`});
@@ -29,11 +33,13 @@ export default function ({token, logoutFn, showNotification}) {
         getData('get-services', data => setAddedServices(data.services), 'fetch services');
         getData('get-sales', data => setAddedSales(data.sales), 'fetch sales');
 
-        const endDate = new Date();
-        const startDate = new Date(endDate.getFullYear(), endDate.getMonth() - 5, endDate.getDate(), 0, 0, 0);
         getData(`get-sale-stats`, data => setSaleToday(data), 'fetch sale stats');
 
     }, []);
+
+    function get_sale_stats_data() {
+        getData(`get-sale-stats?startDate=${startDate}&endDate=${endDate}`, data => setSaleToday(data), 'fetch sale stats');
+    }
 
     return (
         <div className='flex flex-col space-y-2 w-full'>
@@ -49,6 +55,7 @@ export default function ({token, logoutFn, showNotification}) {
             <div className='flex flex-col space-y-1 p-1 mt-2 sm:mx-10'>
                 <FormTitle text={"Sale Stats"} />
                 <div className='flex flex-wrap  justify-center'>
+                    <DateRangePicker startDate={startDate} endDate={endDate} setEndDate={setEndDate} setStartDate={setStartDate} submitCallBack={get_sale_stats_data} />
                     <NumberStatsCard stats={saleToday} />
                 </div>
             </div>
