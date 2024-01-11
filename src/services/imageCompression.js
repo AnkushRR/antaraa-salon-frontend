3d8b0fdf7fc21568cbf51a1f3d504e1967da3937
Ankush <rodewad.ankush29@gmail.com>
2024-01-11 21:33:55 +0530

    Add 'type' parameter to image upload functions
    The 'uploadToServer' and 'handleImageUpload' functions in the imageCompression service have been modified to allow for specifying the 'type' of image being uploaded. This additional parameter has been added to the request payload for the server-side uploading process.

--- src/services/imageCompression.js
+++ src/services/imageCompression.js
@@ -2,13 +2,14 @@ import imageCompression from 'browser-image-compression';
 import {formDataRequest, postRequest} from "../utils/apiHandler.js";
 
 
-export async function uploadToServer(file, token) {
+export async function uploadToServer(file, token, type) {
     const formData = new FormData();
     formData.append('image', file);
 
     try {
         const response = await formDataRequest('/upload-selfie', formData, {
-            'token': token
+            'token': token,
+            'type': type
         });
 
         console.log("upload to server", response);
@@ -18,7 +19,7 @@ export async function uploadToServer(file, token) {
         throw error;
     }
 }
-export async function handleImageUpload(imageFile, token) {
+export async function handleImageUpload(imageFile, token, type) {
     console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
     console.log(`originalFile.size ${imageFile.size / 1024 / 1024} MB`);
 
@@ -32,7 +33,7 @@ export async function handleImageUpload(imageFile, token) {
         const compressedFile = await imageCompression(imageFile, options);
         console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
         console.log(`compressedFile.size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
-        const resp = await uploadToServer(compressedFile, token); // write your own logic
+        const resp = await uploadToServer(compressedFile, token, type); // write your own logic
         console.log("image upload response", JSON.stringify(resp));
         return resp;
     } catch (error) {
